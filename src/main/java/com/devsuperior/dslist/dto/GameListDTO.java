5a711d1df9bd81abd67c3861dca84959ce68d7da
gabrielturato <gabrielturato@outlook.com>
2023-05-11 01:16:04 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public class GameListDTO {
+    private Long id;
+    private String name;
+
+    private GameListDTO(){
+    }
+
+    public GameListDTO(GameList list) {
+        this.id = list.getId();
+        this.name = list.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
